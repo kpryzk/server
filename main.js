21f3816fa17249ceffff6547e75570b6de30503c
kpryzk <kepryzk@gmail.com>
2019-11-21 12:11:41 -0500

    dfg

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const { db } = require('./server/db')
 const app = require('./server')
-const PORT = process.env.PORT || 5885
+const PORT = process.env.PORT || 3000
 
 db.sync().then(() => {
   console.log('db synced')
